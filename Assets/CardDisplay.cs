14864f79b5d693631f18078f8d60425c2f73d8f8
Lasse <lasse.andresen9@gmail.com>
2019-04-27 12:29:02 +0200

    Added card

---
+++ Assets/CardDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public Text moveAmount;
+    public Text attackAmount;
+    public Text flavorText;
+
+    void Start()
+    {
+        moveAmount.text = card.moveAmount;
+        attackAmount.text = card.attackAmount;
+        flavorText.text = card.flavorText;
+    }
+}
